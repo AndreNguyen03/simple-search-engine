5394d48d06c80b32a8dfc3425dc9ea6b8898e8c9
Ngoc Anh <ngocanh030110@gmail.com>
2025-05-27 06:32:35 +0700

    refactor secret

--- backend/crawler_indexer/config.py
+++ backend/crawler_indexer/config.py
@@ -1,5 +1,7 @@
-PG_HOST = "postgres"        # hoặc tên container nếu chạy bằng Docker network
-PG_PORT = 5432
-PG_USER = "myuser"
-PG_PASSWORD = "mypassword"
-PG_DB = "mydb"
+import os
+
+PG_HOST = os.getenv('PG_HOST', 'localhost')
+PG_PORT = int(os.getenv('PG_PORT', 5432))
+PG_USER = os.getenv('PG_USER', 'myuser')
+PG_PASSWORD = os.getenv('PG_PASSWORD', 'mypassword')
+PG_DB = os.getenv('PG_DB', 'mydb')
