14427c007f2afa650126df255482d72ac1687b7a
Ngoc Anh <ngocanh030110@gmail.com>
2025-05-28 13:44:08 +0700

    modify search_api config

--- backend/search_api/config.py
+++ backend/search_api/config.py
@@ -6,4 +6,4 @@ PG_USER = os.getenv('PG_USER', 'admin')
 PG_PASSWORD = os.getenv('PG_PASSWORD', 'securepassword')
 PG_DB = os.getenv('PG_DB', 'myapp')
 
-print(f"Using PostgreSQL config: {PG_HOST}:{PG_PORT}, DB: {PG_DB}, User: {PG_USER}")
+print(f"Using PostgreSQL config is: {PG_HOST}:{PG_PORT}, DB: {PG_DB}, User: {PG_USER}")
